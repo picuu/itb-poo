4aa99fbc819fd1d92673e2d2b3082efe5f38407f
picuu <carloscapoforcada@gmail.com>
2024-01-30 10:46:43 +0100

    create class Cercle

---
+++ src/main/kotlin/Cercle.kt
@@ -0,0 +1,31 @@
+package org.example
+
+class Cercle() {
+    private var radi: Float = 0f
+    private var color: String = "blanc"
+
+    constructor(radi: Float): this() {
+        this.radi = radi
+    }
+
+    constructor(radi: Float, color: String): this() {
+        this.radi = radi
+        this.color = color
+    }
+
+    fun setRadi(radi: Float) {
+        this.radi = radi
+    }
+
+    fun getRadi(): Float {
+        return this.radi
+    }
+
+    fun setColor(color: String) {
+        this.color = color
+    }
+
+    fun getColor(): String {
+        return this.color
+    }
+}
