1e29614b6b7b49705b2f7aaa1e4312f5acfb6278
picuu <carloscapoforcada@gmail.com>
2024-01-30 10:26:03 +0100

    create class Persona

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,20 @@ package org.example
 import java.util.Scanner
 
 fun main() {
+    val p1: Persona = Persona()
+    println(p1.toString())
+    p1.casament()
+    p1.aniversari()
+    println(p1.toString())
 
+    val p2: Persona = Persona("Carlos", "Capó")
+    println(p2.toString())
+
+    val p3: Persona = Persona("Raimon", "Izard", "41752530D", 32, true)
+    println(p3.toString())
+    p3.aniversari()
+    p3.aniversari()
+    p3.aniversari()
+    p3.divorci()
+    println(p3.toString())
 }
---
+++ src/main/kotlin/Persona.kt
@@ -0,0 +1,46 @@
+package org.example
+
+class Persona() {
+    private var dni: String = "dniPersona"
+    private var nom: String = "nomPersona"
+    private var cognoms: String = "cognomsPersona"
+    private var edat: Int = 0
+    private var estatCivil: Boolean = false
+
+    constructor(nom: String, cognoms: String): this() {
+        this.nom = nom
+        this.cognoms = cognoms
+    }
+
+    constructor(nom: String, cognoms: String, dni: String, edat: Int, estatCivil: Boolean): this() {
+        this.nom = nom
+        this.cognoms = cognoms
+        this.dni = dni
+        this.edat = edat
+        this.estatCivil = estatCivil
+    }
+
+    fun aniversari(): Int {
+        this.edat++
+        return this.edat
+    }
+
+    fun casament() {
+        this.estatCivil = true
+    }
+
+    fun divorci() {
+        this.estatCivil = false
+    }
+
+    override fun toString(): String {
+        return """
+            ---------- PERSONA ----------
+            DNI: ${this.dni}
+            NOM: ${this.nom}
+            COGNOMS: ${this.cognoms}
+            EDAT: ${this.edat}
+            ESTAT CIVIL: ${this.estatCivil}
+        """.trimIndent()
+    }
+}
