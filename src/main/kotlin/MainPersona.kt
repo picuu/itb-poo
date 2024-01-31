25c485ed675c39a8c10759dfce410dad7da9c0c7
picuu <carloscapoforcada@gmail.com>
2024-01-31 09:21:28 +0100

    remove invalid Persona class instance

--- src/main/kotlin/MainPersona.kt
+++ src/main/kotlin/MainPersona.kt
@@ -1,14 +1,12 @@
 package org.example
 
 fun main() {
-    val p1: Persona = Persona()
-    println(p1.toString())
-    p1.casament()
-    p1.aniversari()
-    println(p1.toString())
-
     val p2: Persona = Persona("Carlos", "Capó")
     println(p2.toString())
+    p2.aniversari()
+    println(p2.toString())
+
+    println()
 
     val p3: Persona = Persona("Raimon", "Izard", "41752530D", 32, true)
     println(p3.toString())
