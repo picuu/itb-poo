26b65663bcb3539af6b8d60e5a35204d7ba26d4e
picuu <carloscapoforcada@gmail.com>
2024-01-31 09:28:01 +0100

    remove redundant .toString() method call

--- src/main/kotlin/MainPersona.kt
+++ src/main/kotlin/MainPersona.kt
@@ -2,17 +2,17 @@ package org.example
 
 fun main() {
     val p2: Persona = Persona("Carlos", "Capó")
-    println(p2.toString())
+    println(p2)
     p2.aniversari()
-    println(p2.toString())
+    println(p2)
 
     println()
 
-    val p3: Persona = Persona("Raimon", "Izard", "41752530D", 32, true)
-    println(p3.toString())
+    val p3: Persona = Persona("Raimon", "Izard", "41573502D", 32, true)
+    println(p3)
     p3.aniversari()
     p3.aniversari()
     p3.aniversari()
     p3.divorci()
-    println(p3.toString())
+    println(p3)
 }
