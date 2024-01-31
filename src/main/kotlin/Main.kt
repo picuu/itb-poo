146073a4e0a01d24130a0331dfb4d1953ea5505b
picuu <carloscapoforcada@gmail.com>
2024-01-31 08:49:46 +0100

    create functions crearCercle, calcularPermimetre and calcularArea

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 package org.example
 
 fun main() {
-    var darrerCercle:Cercle
+    var darrerCercle:Cercle = Cercle()
 
     do {
         println("1. Crear un cercle d'un radi determinat i d'un color determinat")
@@ -16,6 +16,11 @@ fun main() {
 
         when(opcioUsuari) {
             1 -> darrerCercle = crearCercleDeterminat()
+            2 -> darrerCercle = crearCercle()
+            3 -> println(darrerCercle)
+            4 -> println(PURPLE + "Perímetre de l'últim cercle creat: $YELLOW${roundToFloat(calcularPermimetre(darrerCercle.getRadi()), 2)}\n" + RESET)
+            5 -> println(PURPLE + "Perímetre de l'últim cercle creat: $YELLOW${roundToFloat(calcularArea(darrerCercle.getRadi()), 2)}\n" + RESET)
+            6 -> println("Fins aviat!")
         }
 
     } while (opcioUsuari != 6)
@@ -25,5 +30,21 @@ fun crearCercleDeterminat(): Cercle {
     val radi: Float = readFloat("Introdueix el radi desitgat: ", "Tipus dades incorrecte")
     val color: String = readWord("Introdueix el color desitgat: ", "Tipus dades incorrecte")
 
+    println(BLUE + "S'ha creat un cercle $color amb radi $radi\n" + RESET)
     return Cercle(radi, color)
+}
+
+fun crearCercle(): Cercle {
+    val radi: Float = readFloat("Introdueix el radi desitgat: ", "Tipus dades incorrecte")
+
+    println(BLUE + "S'ha creat un cercle amb radi $radi\n" + RESET)
+    return Cercle(radi)
+}
+
+fun calcularPermimetre(radi: Float): Double {
+    return radi * (Math.PI * 2)
+}
+
+fun calcularArea(radi: Float): Double {
+    return Math.PI * (radi * radi)
 }
