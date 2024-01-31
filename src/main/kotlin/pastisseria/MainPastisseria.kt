45a72c6a306ee77aea0286184b708b1f96af2be2
picuu <carloscapoforcada@gmail.com>
2024-01-31 10:20:20 +0100

    create MainPastisseria.kt and instantiate the Pasta class

---
+++ src/main/kotlin/pastisseria/MainPastisseria.kt
@@ -0,0 +1,11 @@
+package pastisseria
+
+fun main() {
+    val croissant: Pasta = Pasta("croissant", 1.5f, 165, 210)
+    val ensaimada: Pasta = Pasta("ensaimada", 8.25f, 600, 480)
+    val donut: Pasta = Pasta("donut", 2f, 180, 320)
+
+    println(croissant)
+    println(ensaimada)
+    println(donut)
+}
